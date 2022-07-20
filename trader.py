b85174be212f631ec81edc311f2e7e18587299f7
lobodInI <serhiy.loboda@gmail.com>
2022-07-20 21:33:37 +0300

    update. fixed the read and write files

--- trader.py
+++ trader.py
@@ -3,69 +3,63 @@ from os import path
 from random import uniform
 from argparse import ArgumentParser
 
-def create_relevant_config():
-    """Створює новий файл конфігурацій"""
-    with open('config.json', 'r') as json_file:
-        info = json.load(json_file)
-    with open('relevant_config.json', 'w') as new_file_json:
-        json.dump(info, new_file_json)
 
-def existence_check():                                # Додав в кожну функцію, щоб перевірка була завжди.
-    """Перевіряє чи є новий файл конфігурацій"""
-    if path.isfile('relevant_config.json') == False:
-        create_relevant_config()
-
-def open_relevant_config():
-    with open('relevant_config.json', 'r') as json_file:
+def open_file_config(file_name):
+    """Відкривання файлу конфігурації json для читання з заданим ім'ям"""
+    if path.isfile(f'{file_name}.json') == False:
+        with open('config.json', 'r') as json_file:
+            info = json.load(json_file)
+            return info
+    with open(f'{file_name}.json', 'r') as json_file:
         info = json.load(json_file)
     return info
 
-def close_relevant_config(somename):
-    with open('relevant_config.json', 'w') as json_file:
-        json.dump(somename, json_file)
 
+def recording_newfile_config(file_name, something):
+    """збереження в файл конфігурацій json з заданим ім'ям """
+    with open(f'{file_name}.json', 'w') as json_file:
+        json.dump(something, json_file)
+
+def create_config_file():
+    open_file_config(file_name)
+    recording_newfile_config(file_name, something=open_file_config(file_name))
 
 def rate():
     '''Повертає поточний курс валютної пари, в нашому випадку UAH до USD'''
-    existence_check()
-    return open_relevant_config()['exchange_rate']
+    return open_file_config(file_name)['exchange_rate']
 
 
 def available():
     """Повертає поточний стан на рахунку у валютах"""
-    existence_check()
-    return f"USD {open_relevant_config()['USD']} UAH {open_relevant_config()['UAH']}"
+    return f"USD {open_file_config(file_name)['USD']} UAH {open_file_config(file_name)['UAH']}"
 
 
 def buy_xxx(xxx):
     """Купівля валюти(USD) вказаної кількості"""
-    existence_check()
-    info = open_relevant_config()
+    info = open_file_config(file_name)
     recount = round(xxx*info['exchange_rate'], 2)
     if (info['UAH'] - recount) > 0:
         info['UAH'] = round((info['UAH'] - recount), 2)
         info['USD'] = round((info['USD'] + xxx), 2)
-        close_relevant_config(info)
+        recording_newfile_config(file_name, info)
     else:
         return f"UNAVAILABLE, REQUIRED BALANCE UAH {recount}, AVAILABLE {info['UAH']}"
 
 
 def sell_xxx(xxx):
     """Продаж валюти(USD) вказаної кількості"""
-    existence_check()
-    info = open_relevant_config()
+    info = open_file_config(file_name)
     if xxx <= info['USD']:
         recount = xxx * info['exchange_rate']
         info['UAH'] = round((info['UAH'] + recount), 2)
         info['USD'] = round((info['USD'] - xxx), 2)
-        close_relevant_config(info)
+        recording_newfile_config(file_name, info)
     else:
         return f"UNAVAILABLE, REQUIRED BALANCE USD {xxx}, AVAILABLE {info['USD']}"
 
 def buy_all():
     """Купівля валюти (USD) на всі кошти на рахунку (UAH)"""
-    existence_check()
-    info = open_relevant_config()
+    info = open_file_config(file_name)
     if info['UAH'] > 0.01*info['exchange_rate']:
         currency_quantity = round(info['UAH']/info['exchange_rate'], 2)
         info['USD'] = round(info['USD'] + currency_quantity, 2)
@@ -73,35 +67,39 @@ def buy_all():
         if info['UAH'] < 0:
             info['UAH'] = round((info['UAH'] + 0.01*info['exchange_rate']), 2)
             info['USD'] = round(info['USD'] - 0.01, 2)
-        close_relevant_config(info)
+        recording_newfile_config(file_name, info)
     else:
         return f"UNAVAILABLE, AVAILABLE UAH {info['UAH']}"
 
 def sell_all():
     """Продаж всієї валюти (USD) і конвертація у валюту (UAH)"""
-    existence_check()
-    info = open_relevant_config()
+    info = open_file_config(file_name)
     if info['USD'] > 0:
         currency_quantity = info['USD']*info['exchange_rate']
         info['USD'] -= info['USD']
         info['UAH'] = round(info['UAH'] + currency_quantity, 2)
-        close_relevant_config(info)
+        recording_newfile_config(file_name, info)
     else:
         return f"UNAVAILABLE, AVAILABLE  USD {info['USD']}"
 
 
 def next():
     """Зміна поточного курсу в межах діапазону за рахунок 'delta' """
-    existence_check()
-    info = open_relevant_config()
+    # existence_check()
+    info = open_file_config(file_name)
     info['exchange_rate'] = round(uniform(info['exchange_rate']-info['delta'],
                                           info['exchange_rate']+info['delta']), 2)
-    close_relevant_config(info)
+    recording_newfile_config(file_name, info)
 
 def restart():
     """Скидання всіх налаштувань, створення нового файлу з початковими конфігураціями"""
-    create_relevant_config()
+    open_file_config('config')
+    recording_newfile_config(file_name, something=open_file_config('config'))
 
+
+
+file_name = 'relevant_config'
+create_config_file()
 
 args = ArgumentParser()
 args.add_argument('command')
