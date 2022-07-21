ce3b31defd0d078463b1c38e8b97eec977345608
lobodInI <serhiy.loboda@gmail.com>
2022-07-21 23:49:36 +0300

    fixed functions. removed all sorts of nonsense

--- trader.py
+++ trader.py
@@ -1,143 +1,125 @@
 import json
-from os import path
+import os.path
 from random import uniform
 from argparse import ArgumentParser
 
+CONFIG_FILENAME = 'config.json'
+CONFIG_TMP_FILENAME = 'relevant_config.json'
 
-def open_file_config(file_name):
-    """Відкривання файлу конфігурації json для читання з заданим ім'ям"""
-    if path.isfile(f'{file_name}.json') == False:
-        with open('config.json', 'r') as json_file:
-            info = json.load(json_file)
-            return info
-    with open(f'{file_name}.json', 'r') as json_file:
+
+def read_file_config(CONFIG_TMP_FILENAME):
+    """Читання файлу конфігурації json з заданим ім'ям"""
+    with open(CONFIG_TMP_FILENAME, 'r') as json_file:
         info = json.load(json_file)
     return info
 
 
-def recording_file_config(file_name, something):
+def recording_tmp_config(data_config):
     """збереження в файл конфігурацій json з заданим ім'ям """
-    with open(f'{file_name}.json', 'w') as json_file:
-        json.dump(something, json_file)
+    with open(CONFIG_TMP_FILENAME, 'w') as json_file:
+        json.dump(data_config, json_file)
 
-def create_config_file():
+
+def get_config():
     """функція створення нового файлу конфігурації """
-    open_file_config(file_name)
-    recording_file_config(file_name, something=open_file_config(file_name))
-
-def rate():
-    '''Повертає поточний курс валютної пари, в нашому випадку UAH до USD'''
-    return open_file_config(file_name)['exchange_rate']
-
-
-def available():
-    """Повертає поточний стан на рахунку у валютах"""
-    return f"USD {open_file_config(file_name)['USD']} UAH {open_file_config(file_name)['UAH']}"
+    if os.path.isfile(CONFIG_TMP_FILENAME):
+        info = read_file_config(CONFIG_TMP_FILENAME)
+    else:
+        info = restart()
+    return info
 
 
-def buy_xxx(xxx):
+def rate(info):
+    """Поточний курс валютної пари, в нашому випадку UAH до USD"""
+    print(info['exchange_rate'])
+
+
+def available(info):
+    """Поточний стан на рахунку у валютах"""
+    print(f"USD {info['USD']} UAH {info['UAH']}")
+
+
+def buy_xxx(usd_amount, info):
     """Купівля валюти(USD) вказаної кількості"""
-    info = open_file_config(file_name)
-    recount = round(xxx*info['exchange_rate'], 2)
+    recount = round(usd_amount * info['exchange_rate'], 2)
     if (info['UAH'] - recount) > 0:
         info['UAH'] = round((info['UAH'] - recount), 2)
-        info['USD'] = round((info['USD'] + xxx), 2)
-        recording_file_config(file_name, info)
+        info['USD'] = round((info['USD'] + usd_amount), 2)
+        recording_tmp_config(info)
     else:
-        return f"UNAVAILABLE, REQUIRED BALANCE UAH {recount}, AVAILABLE {info['UAH']}"
+        print(f"UNAVAILABLE, REQUIRED BALANCE UAH {recount}, AVAILABLE {info['UAH']}")
 
 
-def sell_xxx(xxx):
+def sell_xxx(usd_amount, info):
     """Продаж валюти(USD) вказаної кількості"""
-    info = open_file_config(file_name)
-    if xxx <= info['USD']:
-        recount = xxx * info['exchange_rate']
+    if usd_amount <= info['USD']:
+        recount = usd_amount * info['exchange_rate']
         info['UAH'] = round((info['UAH'] + recount), 2)
-        info['USD'] = round((info['USD'] - xxx), 2)
-        recording_file_config(file_name, info)
+        info['USD'] = round((info['USD'] - usd_amount), 2)
+        recording_tmp_config(info)
     else:
-        return f"UNAVAILABLE, REQUIRED BALANCE USD {xxx}, AVAILABLE {info['USD']}"
+        print(f"UNAVAILABLE, REQUIRED BALANCE USD {usd_amount}, AVAILABLE {info['USD']}")
 
-def buy_all():
+
+def buy_all(info):
     """Купівля валюти (USD) на всі кошти на рахунку (UAH)"""
-    info = open_file_config(file_name)
-    if info['UAH'] > 0.01*info['exchange_rate']:
-        currency_quantity = round(info['UAH']/info['exchange_rate'], 2)
+    if info['UAH'] > 0.01 * info['exchange_rate']:
+        currency_quantity = round(info['UAH'] / info['exchange_rate'], 2)
         info['USD'] = round(info['USD'] + currency_quantity, 2)
-        info['UAH'] = round(info['UAH'] - currency_quantity*info['exchange_rate'], 2)
+        info['UAH'] = round(info['UAH'] - currency_quantity * info['exchange_rate'], 2)
         if info['UAH'] < 0:
-            info['UAH'] = round((info['UAH'] + 0.01*info['exchange_rate']), 2)
+            info['UAH'] = round((info['UAH'] + 0.01 * info['exchange_rate']), 2)
             info['USD'] = round(info['USD'] - 0.01, 2)
-        recording_file_config(file_name, info)
-    else:
-        return f"UNAVAILABLE, AVAILABLE UAH {info['UAH']}"
+        recording_tmp_config(info)
 
-def sell_all():
+
+def sell_all(info):
     """Продаж всієї валюти (USD) і конвертація у валюту (UAH)"""
-    info = open_file_config(file_name)
     if info['USD'] > 0:
-        currency_quantity = info['USD']*info['exchange_rate']
+        currency_quantity = info['USD'] * info['exchange_rate']
         info['USD'] -= info['USD']
         info['UAH'] = round(info['UAH'] + currency_quantity, 2)
-        recording_file_config(file_name, info)
-    else:
-        return f"UNAVAILABLE, AVAILABLE  USD {info['USD']}"
+        recording_tmp_config(info)
 
 
-def next():
+def next(info):
     """Зміна поточного курсу в межах діапазону за рахунок 'delta' """
-    # existence_check()
-    info = open_file_config(file_name)
-    info['exchange_rate'] = round(uniform(info['exchange_rate']-info['delta'],
-                                          info['exchange_rate']+info['delta']), 2)
-    recording_file_config(file_name, info)
+    info['exchange_rate'] = round(uniform(info['exchange_rate'] - info['delta'],
+                                          info['exchange_rate'] + info['delta']), 2)
+    recording_tmp_config(info)
+
 
 def restart():
-    """Скидання всіх налаштувань, створення нового файлу з початковими конфігураціями"""
-    open_file_config('config')
-    recording_file_config(file_name, something=open_file_config('config'))
+    """Створення нового файлу з початковими конфігураціями"""
+    info = read_file_config(CONFIG_FILENAME)
+    recording_tmp_config(info)
+    return info
 
-
-
-file_name = 'relevant_config'
-create_config_file()
 
 args = ArgumentParser()
 args.add_argument('command')
-args.add_argument('second_argument', type=str, nargs='?', default=0)    # створив другий аргумент типу "універсальній"
-args = vars(args.parse_args())                                          # (не знаю наскільки це дозволено)
+args.add_argument('second_argument', type=str, nargs='?', default=0)  # створив другий аргумент типу "універсальній"
+args = vars(args.parse_args())
 command = args['command']
-# command = args['command'].upper()                          # написав, бо чомусь не працює верхній регістр у терміналі
 second_argument = args['second_argument']
-# second_argument = args['second_argument'].upper()          # написав, бо чомусь не працює верхній регістр у терміналі
+
+info = get_config()
+
 if command == 'RATE':
-    variable = rate()
-    print(variable)
+    rate(info)
 elif command == 'AVAILABLE':
-    variable = available()
-    print(variable)
+    available(info)
 elif command == 'BUY':
     if second_argument == 'ALL':
-        variable = buy_all()
-        print(variable) if variable != None else None       # якщо без тернарного оператора прінтує None
+        buy_all(info)
     else:
-        second_argument = float(second_argument)            # відбувається перетворення, якщо ввели число а не "ALL"
-        variable = buy_xxx(second_argument)
-        print(variable) if variable != None else None
+        buy_xxx(float(second_argument), info)
 elif command == 'SELL':
     if second_argument == 'ALL':
-        variable = sell_all()
-        print(variable) if variable != None else None
+        sell_all(info)
     else:
-        second_argument = float(second_argument)            # відбувається перетворення, якщо ввели число а не "ALL"
-        variable = sell_xxx(second_argument)
-        print(variable) if variable != None else None
+        sell_xxx(float(second_argument), info)
 elif command == 'NEXT':
-    next()
+    next(info)
 elif command == 'RESTART':
     restart()
-
-
-
-
-
